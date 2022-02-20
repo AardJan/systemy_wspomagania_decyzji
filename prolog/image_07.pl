0894b0b7a4ebbb36156494d1e100a2b3dc93cb5b
Adrjan <43217550+AardJan@users.noreply.github.com>
2022-02-20 10:36:31 +0100

    Create image_07.pl

---
+++ prolog/image_07.pl
@@ -0,0 +1,13 @@
+%% facts
+have(room, wall).
+have(room, furniture).
+have(wall, window).
+have(window, blind).
+
+have(furniture, table).
+have(furniture, carpet).
+have(furniture, bed).
+have(bed, quilt).
+have(table, legs).
+have(table, tabletop).
+ 
