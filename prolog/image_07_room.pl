6e7a632ff54eb6d18cdf7b1ba532ff5ed51c096b
Adrjan <43217550+AardJan@users.noreply.github.com>
2022-02-27 12:18:03 +0100

    rename file

--- prolog/image_07_room.pl
+++ prolog/image_07_room.pl
@@ -1,3 +1,20 @@
+%% atoms
+
+room.
+wall.
+furniture.
+window.
+blind.
+table.
+carpet.
+bed.
+quilt.
+legs.
+tabletop.
+closet.
+
+
+
 %% facts
 have(room, wall).
 have(room, furniture).
@@ -7,7 +24,10 @@ have(window, blind).
 have(furniture, table).
 have(furniture, carpet).
 have(furniture, bed).
+have(furniture, closet).
 have(bed, quilt).
 have(table, legs).
 have(table, tabletop).
- 
+
+can(window, open).
+can()
