a023f32b100ce66de29c9adea1b904b8c5be7c47
Adrjan <43217550+AardJan@users.noreply.github.com>
2022-02-27 12:41:58 +0100

    Create dynasty_01.pl

---
+++ prolog/dynasty_01.pl
@@ -0,0 +1,59 @@
+%% atoms
+kazimierz_ii_sprawiedliwy.
+leszek_bialy.
+konrad_i_mazowiecki.
+boleslaw_v_wstydliwy.
+kazimierz_i.
+siemowit.
+leszek_czarny.
+siemomysl.
+wladyslaw_i_lokietek.
+kazimierz_ii.
+siemowit_kzi.
+leszek.
+przemysl.
+kazimierz_iii.
+kazimierz_iii_wielki.
+wladyslaw_garbaty.
+boleslaw.
+wladyslaw_bialy.
+
+
+%% facts
+person(kazimierz_ii_sprawiedliwy).
+person(leszek_bialy).
+person(konrad_i_mazowiecki).
+person(boleslaw_v_wstydliwy).
+person(kazimierz_i).
+person(siemowit).
+person(leszek_czarny).
+person(siemomysl).
+person(wladyslaw_i_lokietek).
+person(kazimierz_ii).
+person(siemowit_kzi).
+person(leszek).
+person(przemysl).
+person(kazimierz_iii).
+person(kazimierz_iii_wielki).
+person(wladyslaw_garbaty).
+person(boleslaw).
+person(wladyslaw_bialy).
+
+child(kazimierz_ii_sprawiedliwy, leszek_bialy).
+child(kazimierz_ii_sprawiedliwy, konrad_i_mazowiecki).
+child(konrad_i_mazowiecki, boleslaw).
+child(konrad_i_mazowiecki, kazimierz_i).
+child(konrad_i_mazowiecki, siemowit).
+child(kazimierz_i, boleslaw_v_wstydliwy).
+child(kazimierz_i, leszek_czarny).
+child(kazimierz_i, siemomysl).
+child(kazimierz_i, kazimierz_ii).
+child(kazimierz_i, siemowit_kzi).
+child(siemomysl, leszek).
+child(siemomysl, przemysl).
+child(siemomysl, kazimierz_iii).
+child(kazimierz_iii, wladyslaw_bialy).
+child(wladyslaw_i_lokietek, kazimierz_iii_wielki).
+child(siemowit_kzi, wladyslaw_garbaty).
+child(siemowit_kzi, boleslaw).
+
