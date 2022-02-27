0eb7cc308963973facede3eecdb26df17bdde291
Adrjan <43217550+AardJan@users.noreply.github.com>
2022-02-27 12:28:39 +0100

    Create image_01_plane.pl

---
+++ prolog/image_01_plane.pl
@@ -0,0 +1,31 @@
+%% atoms
+coverage.
+doors.
+gates.
+windows.
+cockpit.
+wings.
+lamps.
+insciption.
+image.
+
+fly.
+take_off.
+landing.
+crush.
+
+%% facts
+have(coverage).
+have(doors).
+have(gates).
+have(windows).
+have(lamps).
+have(wings).
+have(cockpit).
+have(insciption).
+have(image).
+
+can(fly).
+can(take_off).
+can(landing).
+can(crush).
